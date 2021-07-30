de40d1a87b58fabbb81b6507ee8892e9e2d5ba6b
Mubashir01234 <omerkhan01234@gmail.com>
2021-07-30 18:04:58 +0500

    first push

---
+++ src/library/management/system/Signup.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library.management.system;
+
+import java.awt.*;
+import javax.swing.*;
+import java.sql.*;
+import java.awt.event.*;
+import javax.swing.border.*;
+
+public class Signup extends JFrame implements ActionListener{
+
+    private JPanel contentPane;
+    private JTextField textField;
+    private JTextField textField_1;
+    private JTextField textField_2;
+    private JTextField textField_3;
+    private JButton b1, b2;
+    private JComboBox comboBox;
+
+
+    public static void main(String[] args) {
+        new Signup().setVisible(true);
+    }
+
+    public Signup() {
+        setBounds(600, 250, 606, 406);
+	contentPane = new JPanel();
+	contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+	setContentPane(contentPane);
+        contentPane.setBackground(Color.WHITE);
+	contentPane.setLayout(null);
+
+	JLabel lblUsername = new JLabel("Username :");
+	lblUsername.setForeground(Color.DARK_GRAY);
+	lblUsername.setFont(new Font("Tahoma", Font.BOLD, 14));
+	lblUsername.setBounds(99, 118, 92, 26);
+	contentPane.add(lblUsername);
+
+	JLabel lblPassword = new JLabel("Password :");  
+	lblPassword.setForeground(Color.DARK_GRAY);
+	lblPassword.setFont(new Font("Tahoma", Font.BOLD, 14));
+	lblPassword.setBounds(99, 160, 92, 26);
+	contentPane.add(lblPassword);
+
+	textField_1 = new JTextField();
+	textField_1.setColumns(10);
+	textField_1.setBounds(265, 128, 148, 20);
+	contentPane.add(textField_1);
+
+        textField_2 = new JTextField();
+	textField_2.setColumns(10);
+	textField_2.setBounds(265, 165, 148, 20);
+	contentPane.add(textField_2);
+
+	
+	b1 = new JButton("Create");
+	b1.addActionListener(this);
+	b1.setFont(new Font("Tahoma", Font.BOLD, 13));
+	b1.setBounds(140, 289, 100, 30);
+        b1.setBackground(Color.BLACK);
+        b1.setForeground(Color.WHITE);
+	contentPane.add(b1);
+
+	b2 = new JButton("Back");
+	b2.addActionListener(this);
+	b2.setFont(new Font("Tahoma", Font.BOLD, 13));
+	b2.setBounds(300, 289, 100, 30);
+	b2.setBackground(Color.BLACK);
+        b2.setForeground(Color.WHITE);
+	contentPane.add(b2);
+
+	JPanel panel = new JPanel();
+	panel.setForeground(new Color(34, 139, 34));
+	panel.setBorder(new TitledBorder(new LineBorder(new Color(128, 128, 0), 2), "Create-Account",
+			TitledBorder.LEADING, TitledBorder.TOP, null, new Color(34, 139, 34)));
+	panel.setBounds(31, 46, 476, 296);
+        panel.setBackground(Color.WHITE);
+	contentPane.add(panel);
+    }
+    
+    public void actionPerformed(ActionEvent ae){
+        String id = textField_1.getText();
+        String nam = textField_2.getText();
+        
+        try{
+            conn con = new conn();
+            
+            if(ae.getSource() == b1){
+                try{                                                                            
+                String myQry = "insert into login values('"+id+"','"+nam+"')";
+                con.s.executeUpdate(myQry);                        
+            }catch(Exception ee){                    
+                ee.printStackTrace();   
+            } 
+                
+                    JOptionPane.showMessageDialog(null, "successfully Created");
+            
+                textField.setText("");
+                textField_1.setText("");
+		textField_2.setText("");
+		textField_3.setText("");
+            }
+            if(ae.getSource() == b2){
+                this.setVisible(false);
+		new Login_user().setVisible(true);
+			
+            }
+        }catch(Exception e){
+            
+        }
+    }
+}
+
